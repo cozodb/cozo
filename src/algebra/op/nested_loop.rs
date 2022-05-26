0fe284fe8d22467305e7a2ecfd69956dcd71f804
Ziyang Hu <hu.ziyang@cantab.net>
2022-05-26 22:47:09 +0800

    more of nested loops

--- src/algebra/op/nested_loop.rs
+++ src/algebra/op/nested_loop.rs
@@ -1,29 +1,51 @@
-use crate::algebra::op::RelationalAlgebra;
+use crate::algebra::op::{build_binding_map_from_info, RelationalAlgebra};
 use crate::algebra::parser::RaBox;
-use crate::data::tuple_set::{BindingMap, TupleSet};
+use crate::context::TempDbContext;
+use crate::data::tuple_set::{shift_merge_binding_map, BindingMap, TupleSet};
 use crate::ddl::reify::TableInfo;
 use anyhow::Result;
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 
 pub(crate) const NAME_NESTED_LOOP_LEFT: &str = "NestedLoop";
 
 pub(crate) struct NestedLoopLeft<'a> {
+    pub(crate) ctx: &'a TempDbContext<'a>,
     pub(crate) left: RaBox<'a>,
     pub(crate) right: TableInfo,
+    pub(crate) right_binding: String,
     pub(crate) left_outer_join: bool,
+}
+
+fn nested_binding(left: &RaBox, binding: &str) -> Result<BTreeSet<String>> {
+    let mut bindings = left.bindings()?;
+    bindings.insert(binding.to_string());
+    Ok(bindings)
+}
+
+fn nested_binding_map(
+    ctx: &TempDbContext,
+    left: &RaBox,
+    right_info: &TableInfo,
+    right_binding: &str,
+) -> Result<BindingMap> {
+    let mut binding_map = left.binding_map()?;
+    let right = build_binding_map_from_info(ctx, right_info, &[])?;
+    let right_map = BTreeMap::from([(right_binding.to_string(), right)]);
+    shift_merge_binding_map(&mut binding_map, right_map);
+    Ok(binding_map)
 }
 
 impl<'b> RelationalAlgebra for NestedLoopLeft<'b> {
     fn name(&self) -> &str {
-        todo!()
+        NAME_NESTED_LOOP_LEFT
     }
 
     fn bindings(&self) -> Result<BTreeSet<String>> {
-        todo!()
+        nested_binding(&self.left, &self.right_binding)
     }
 
     fn binding_map(&self) -> Result<BindingMap> {
-        todo!()
+        nested_binding_map(self.ctx, &self.left, &self.right, &self.right_binding)
     }
 
     fn iter<'a>(&'a self) -> Result<Box<dyn Iterator<Item = Result<TupleSet>> + 'a>> {
@@ -31,28 +53,30 @@ impl<'b> RelationalAlgebra for NestedLoopLeft<'b> {
     }
 
     fn identity(&self) -> Option<TableInfo> {
-        todo!()
+        None
     }
 }
 
 pub(crate) const NAME_NESTED_LOOP_RIGHT: &str = "NestedLoopRight";
 
 pub(crate) struct NestedLoopRight<'a> {
+    pub(crate) ctx: &'a TempDbContext<'a>,
     pub(crate) left: RaBox<'a>,
     pub(crate) right: TableInfo,
+    pub(crate) right_binding: String,
 }
 
 impl<'b> RelationalAlgebra for NestedLoopRight<'b> {
     fn name(&self) -> &str {
-        todo!()
+        NAME_NESTED_LOOP_RIGHT
     }
 
     fn bindings(&self) -> Result<BTreeSet<String>> {
-        todo!()
+        nested_binding(&self.left, &self.right_binding)
     }
 
     fn binding_map(&self) -> Result<BindingMap> {
-        todo!()
+        nested_binding_map(self.ctx, &self.left, &self.right, &self.right_binding)
     }
 
     fn iter<'a>(&'a self) -> Result<Box<dyn Iterator<Item = Result<TupleSet>> + 'a>> {
@@ -60,28 +84,30 @@ impl<'b> RelationalAlgebra for NestedLoopRight<'b> {
     }
 
     fn identity(&self) -> Option<TableInfo> {
-        todo!()
+        None
     }
 }
 
 pub(crate) const NAME_NESTED_LOOP_OUTER: &str = "NestedLoopOuter";
 
 pub(crate) struct NestedLoopOuter<'a> {
+    pub(crate) ctx: &'a TempDbContext<'a>,
     pub(crate) left: RaBox<'a>,
     pub(crate) right: TableInfo,
+    pub(crate) right_binding: String,
 }
 
 impl<'b> RelationalAlgebra for NestedLoopOuter<'b> {
     fn name(&self) -> &str {
-        todo!()
+        NAME_NESTED_LOOP_OUTER
     }
 
     fn bindings(&self) -> Result<BTreeSet<String>> {
-        todo!()
+        nested_binding(&self.left, &self.right_binding)
     }
 
     fn binding_map(&self) -> Result<BindingMap> {
-        todo!()
+        nested_binding_map(self.ctx, &self.left, &self.right, &self.right_binding)
     }
 
     fn iter<'a>(&'a self) -> Result<Box<dyn Iterator<Item = Result<TupleSet>> + 'a>> {
@@ -89,6 +115,6 @@ impl<'b> RelationalAlgebra for NestedLoopOuter<'b> {
     }
 
     fn identity(&self) -> Option<TableInfo> {
-        todo!()
+        None
     }
 }
