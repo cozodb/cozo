ce5c2870f652c4b4a1c25da249d2f4bd160cb833
Ziyang Hu <hu.ziyang@cantab.net>
2022-10-30 22:05:46 +0800

    linux build script

--- build-release-linux.sh
+++ build-release-linux.sh
@@ -3,6 +3,7 @@ set -e
 
 VERSION=$(cat ./Cargo.toml | grep -E "^version" | grep -Eo '[0-9.]+')
 
+rm -fr release
 mkdir release
 
 cross build --target=aarch64-unknown-linux-gnu --release
@@ -10,7 +11,7 @@ cross build --target=x86_64-unknown-linux-gnu --release
 
 cp target/aarch64-unknown-linux-gnu/release/cozoserver release/cozoserver-${VERSION}-linux-arm64
 cp target/x86_64-unknown-linux-gnu/release/cozoserver release/cozoserver-${VERSION}-linux-x86_64
-podman run --rm -v $PWD:/work ghcr.io/cross-rs/aarch64-unknown-linux-gnu:main strip /work/release/release/cozoserver-${VERSION}-linux-arm64
-podman run --rm -v $PWD:/work ghcr.io/cross-rs/x86_64-unknown-linux-gnu:main strip /work/release/release/cozoserver-${VERSION}-linux-x86_64
+podman run --rm -v $PWD:/work ghcr.io/cross-rs/aarch64-unknown-linux-gnu:main strip /work/release/cozoserver-${VERSION}-linux-arm64
+podman run --rm -v $PWD:/work ghcr.io/cross-rs/x86_64-unknown-linux-gnu:main strip /work/release/cozoserver-${VERSION}-linux-x86_64
 gzip release/cozoserver-${VERSION}-linux-arm64
 gzip release/cozoserver-${VERSION}-linux-x86_64
