ab19c2ca00aef137e4efa49027e4ef1f51908d53
Ziyang Hu <hu.ziyang@cantab.net>
2022-10-30 22:04:54 +0800

    linux build script

--- build-release-linux.sh
+++ build-release-linux.sh
@@ -10,7 +10,7 @@ cross build --target=x86_64-unknown-linux-gnu --release
 
 cp target/aarch64-unknown-linux-gnu/release/cozoserver release/cozoserver-${VERSION}-linux-arm64
 cp target/x86_64-unknown-linux-gnu/release/cozoserver release/cozoserver-${VERSION}-linux-x86_64
-podman run --rm -v $PWD:/work ghcr.io/cross-rs/aarch64-unknown-linux-gnu:main "strip /work/release/release/cozoserver-${VERSION}-linux-arm64"
-podman run --rm -v $PWD:/work ghcr.io/cross-rs/x86_64-unknown-linux-gnu:main "strip /work/release/release/cozoserver-${VERSION}-linux-x86_64"
+podman run --rm -v $PWD:/work ghcr.io/cross-rs/aarch64-unknown-linux-gnu:main strip /work/release/release/cozoserver-${VERSION}-linux-arm64
+podman run --rm -v $PWD:/work ghcr.io/cross-rs/x86_64-unknown-linux-gnu:main strip /work/release/release/cozoserver-${VERSION}-linux-x86_64
 gzip release/cozoserver-${VERSION}-linux-arm64
 gzip release/cozoserver-${VERSION}-linux-x86_64
