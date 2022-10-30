da30d80fe54995d2a1c1cdae73b2100af70dbe4e
Ziyang Hu <hu.ziyang@cantab.net>
2022-10-30 22:00:32 +0800

    linux build script

--- build-release-linux.sh
+++ build-release-linux.sh
@@ -10,5 +10,7 @@ cross build --target=x86_64-unknown-linux-gnu --release
 
 cp target/aarch64-unknown-linux-gnu/release/cozoserver release/cozoserver-${VERSION}-linux-arm64
 cp target/x86_64-unknown-linux-gnu/release/cozoserver release/cozoserver-${VERSION}-linux-x86_64
+strip release/cozoserver-${VERSION}-linux-arm64
+strip release/cozoserver-${VERSION}-linux-x86_64
 gzip release/cozoserver-${VERSION}-linux-arm64
 gzip release/cozoserver-${VERSION}-linux-x86_64
