83e1c672f47a818faec860078c3d97c9f3ce17de
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-02 21:36:58 +0800

    windows build file

--- build-release-windows.ps1
+++ build-release-windows.ps1
@@ -13,8 +13,15 @@ if (test-path release) {
 mkdir release
 
 cargo build --release
+cargo build --release --manifest-path=cozo-lib-c/Cargo.toml
 
 cp target/release/cozoserver.exe release/cozoserver-${COZO_VERSION}-windows-x86_64.exe
+cp target/release/cozo_c.dll release/libcozo_c-${COZO_VERSION}-windows-x86_64.dll
+cp target/release/cozo_c.lib release/libcozo_c-${COZO_VERSION}-windows-x86_64.lib
 
 Compress-Archive -Path release/cozoserver-${COZO_VERSION}-windows-x86_64.exe -DestinationPath release/cozoserver-${COZO_VERSION}-windows-x86_64.zip
+Compress-Archive -Path release/libcozo_c-${COZO_VERSION}-windows-x86_64.dll -DestinationPath release/libcozo_c-${COZO_VERSION}-windows-x86_64.dll
+Compress-Archive -Path release/libcozo_c-${COZO_VERSION}-windows-x86_64.lib -DestinationPath release/libcozo_c-${COZO_VERSION}-windows-x86_64.lib
 Remove-Item release/cozoserver-${COZO_VERSION}-windows-x86_64.exe
+Remove-Item release/libcozo_c-${COZO_VERSION}-windows-x86_64.dll
+Remove-Item release/libcozo_c-${COZO_VERSION}-windows-x86_64.lib
