dc04165df68da83fa2a62afd7f19516c37c3462c
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-02 21:42:57 +0800

    mac build

--- build-release-mac.sh
+++ build-release-mac.sh
@@ -17,4 +17,3 @@ cp target/release/libcozo_c.dylib release/libcozo_c-${VERSION}-mac-${ARCH}.dylib
 strip release/cozoserver-${VERSION}-mac-${ARCH}
 
 gzip release/*
-cp cozo-lib-c/cozo_c.h release/cozo_c.h
