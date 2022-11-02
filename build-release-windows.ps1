338ece962a694b8edcb401967a3d86a1ee43d0cd
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-02 21:37:37 +0800

    windows build file

--- build-release-windows.ps1
+++ build-release-windows.ps1
@@ -20,8 +20,8 @@ cp target/release/cozo_c.dll release/libcozo_c-${COZO_VERSION}-windows-x86_64.dl
 cp target/release/cozo_c.lib release/libcozo_c-${COZO_VERSION}-windows-x86_64.lib
 
 Compress-Archive -Path release/cozoserver-${COZO_VERSION}-windows-x86_64.exe -DestinationPath release/cozoserver-${COZO_VERSION}-windows-x86_64.zip
-Compress-Archive -Path release/libcozo_c-${COZO_VERSION}-windows-x86_64.dll -DestinationPath release/libcozo_c-${COZO_VERSION}-windows-x86_64.dll
-Compress-Archive -Path release/libcozo_c-${COZO_VERSION}-windows-x86_64.lib -DestinationPath release/libcozo_c-${COZO_VERSION}-windows-x86_64.lib
+Compress-Archive -Path release/libcozo_c-${COZO_VERSION}-windows-x86_64.dll -DestinationPath release/libcozo_c-${COZO_VERSION}-windows-x86_64.dll.zip
+Compress-Archive -Path release/libcozo_c-${COZO_VERSION}-windows-x86_64.lib -DestinationPath release/libcozo_c-${COZO_VERSION}-windows-x86_64.lib.zip
 Remove-Item release/cozoserver-${COZO_VERSION}-windows-x86_64.exe
 Remove-Item release/libcozo_c-${COZO_VERSION}-windows-x86_64.dll
 Remove-Item release/libcozo_c-${COZO_VERSION}-windows-x86_64.lib
