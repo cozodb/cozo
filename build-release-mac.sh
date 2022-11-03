a4a3b5be42560fe60ad6bdabbede555d92a0030f
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-03 21:00:04 +0800

    move java lib back

--- build-release-mac.sh
+++ build-release-mac.sh
@@ -10,10 +10,12 @@ mkdir release
 
 cargo build --release
 cargo build --release --manifest-path=cozo-lib-c/Cargo.toml
+cargo build --release --manifest-path=cozo-lib-java/Cargo.toml
 
 cp target/release/cozoserver release/cozoserver-${VERSION}-mac-${ARCH}
 cp target/release/libcozo_c.a release/libcozo_c-${VERSION}-mac-${ARCH}.a
 cp target/release/libcozo_c.dylib release/libcozo_c-${VERSION}-mac-${ARCH}.dylib
+cp target/release/libcozo_java.dylib release/libcozo_java-${VERSION}-mac-${ARCH}.dylib
 strip release/cozoserver-${VERSION}-mac-${ARCH}
 
 gzip release/*
