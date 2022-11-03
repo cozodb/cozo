9483602134b1112a7c323dc4c30c830187fb63ad
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-03 21:25:11 +0800

    no linux-aarch64 for java

--- build-release-linux.sh
+++ build-release-linux.sh
@@ -8,7 +8,6 @@ mkdir release
 
 cross build --target=aarch64-unknown-linux-gnu --release
 cross build --target=aarch64-unknown-linux-gnu --release --manifest-path=cozo-lib-c/Cargo.toml
-cross build --target=aarch64-unknown-linux-gnu --release --manifest-path=cozo-lib-java/Cargo.toml
 cross build --target=x86_64-unknown-linux-gnu --release
 cross build --target=x86_64-unknown-linux-gnu --release --manifest-path=cozo-lib-c/Cargo.toml
 cross build --target=x86_64-unknown-linux-gnu --release --manifest-path=cozo-lib-java/Cargo.toml
@@ -16,7 +15,6 @@ cross build --target=x86_64-unknown-linux-gnu --release --manifest-path=cozo-lib
 cp target/aarch64-unknown-linux-gnu/release/cozoserver release/cozoserver-${VERSION}-linux-arm64
 cp target/aarch64-unknown-linux-gnu/release/libcozo_c.a release/libcozo_c-${VERSION}-linux-arm64.a
 cp target/aarch64-unknown-linux-gnu/release/libcozo_c.so release/libcozo_c-${VERSION}-linux-arm64.so
-cp target/aarch64-unknown-linux-gnu/release/libcozo_java.so release/libcozo_java-${VERSION}-linux-arm64.so
 cp target/x86_64-unknown-linux-gnu/release/cozoserver release/cozoserver-${VERSION}-linux-x86_64
 cp target/x86_64-unknown-linux-gnu/release/libcozo_c.a release/libcozo_c-${VERSION}-linux-x86_64.a
 cp target/x86_64-unknown-linux-gnu/release/libcozo_c.so release/libcozo_c-${VERSION}-linux-x86_64.so
