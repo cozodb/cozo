8d71655164c4de0ee1b35f0398b52c2aa54c9719
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-19 23:46:03 +0800

    build script for JNI

--- cozo-lib-java/build.sh
+++ cozo-lib-java/build.sh
@@ -4,3 +4,5 @@ cross build -p cozo_java --release --target=aarch64-linux-android
 cross build -p cozo_java --release --target=armv7-linux-androideabi
 cross build -p cozo_java --release --target=i686-linux-android
 cross build -p cozo_java --release --target=x86_64-linux-android
+cross build -p cozo_java --release -F storage-rocksdb --target=aarch64-unknown-linux-gnu
+cross build -p cozo_java --release -F storage-rocksdb --target=x86_64-unknown-linux-gnu
