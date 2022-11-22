4f0c0e266d59aeafad75f128b41dd82c1eecec7d
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-22 22:30:39 +0800

    script to sign artefacts

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -8,3 +8,7 @@ for f in release/*.exe release/*.dll release/*.lib; do
 done
 
 gzip release/*.a release/*.so release/*.dylib release/*-darwin release/*-gnu release/*-musl
+
+for f in release/*; do
+  gpg --armor --detach-sign $f
+done
