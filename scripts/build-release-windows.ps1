ab497b00de8a5204c1d05c9a929ddec452c859c6
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-25 13:32:53 +0800

    windows build

--- scripts/build-release-windows.ps1
+++ scripts/build-release-windows.ps1
@@ -1,11 +1,15 @@
 $ErrorActionPreference="Stop"
 
 $VERSION = cat .\VERSION
-$CARGO_PROFILE_RELEASE_LTO = "fat"
+$env:CARGO_PROFILE_RELEASE_LTO = "fat"
 $TARGET = "x86_64-pc-windows-msvc"
-$env:PYO3_NO_PYTHON = 1 
+# $env:PYO3_NO_PYTHON = 1
 
 mkdir -force release > $null
+
+cd cozo-lib-python
+maturin build -F compact -F storage-rocksdb --release --strip --target $TARGET
+cd ..
 
 cargo build --release -p cozoserver -p cozo_c -p cozo_java -p cozo-node -F compact -F storage-rocksdb --target $TARGET
 cp target/$TARGET/release/cozoserver.exe release/cozoserver-$VERSION-$TARGET.exe # standalone
@@ -13,9 +17,5 @@ cp target/$TARGET/release/cozo_c.lib release/libcozo_c-$VERSION-$TARGET.lib # c 
 cp target/$TARGET/release/cozo_c.dll release/libcozo_c-$VERSION-$TARGET.dll # c dynamic
 cp target/$TARGET/release/cozo_java.dll release/libcozo_java-$VERSION-$TARGET.dll # java
 cp target/$TARGET/release/cozo_node.dll release/libcozo_node-$VERSION-$TARGET.dll # nodejs
-
-cd cozo-lib-python
-maturin build -F compact -F storage-rocksdb --release --strip --target $TARGET
-cd ..
 
 cp target/wheels/*.whl release/
