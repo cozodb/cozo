1ac412be9bf84cda4c5aece56a2f4c733cbed5ca
Ziyang Hu <hu.ziyang@cantab.net>
2022-12-21 11:00:19 +0800

    fix node module name

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -21,41 +21,41 @@ NODE_DIR_INNER=cozo-lib-nodejs/build/stage/$VERSION/6
 
 rm -fr $NODE_DIR_INNER
 mkdir -p $NODE_DIR_INNER
-cp release/libcozo_node-$VERSION-aarch64-apple-darwin.dylib.gz $NODE_DIR_INNER/index.node.gz
+cp release/libcozo_node-$VERSION-aarch64-apple-darwin.dylib.gz $NODE_DIR_INNER/cozo_node_prebuilt.gz
 pushd $NODE_DIR
-gunzip 6/index.node.gz
+gunzip 6/cozo_node_prebuilt.gz
 tar cvzf 6-darwin-arm64.tar.gz 6/
 popd
 
 rm -fr $NODE_DIR_INNER
 mkdir -p $NODE_DIR_INNER
-cp release/libcozo_node-$VERSION-x86_64-apple-darwin.dylib.gz $NODE_DIR_INNER/index.node.gz
+cp release/libcozo_node-$VERSION-x86_64-apple-darwin.dylib.gz $NODE_DIR_INNER/cozo_node_prebuilt.gz
 pushd $NODE_DIR
-gunzip 6/index.node.gz
+gunzip 6/cozo_node_prebuilt.gz
 tar cvzf 6-darwin-x64.tar.gz 6/
 popd
 
 rm -fr $NODE_DIR_INNER
 mkdir -p $NODE_DIR_INNER
-cp release/libcozo_node-$VERSION-x86_64-unknown-linux-gnu.so.gz $NODE_DIR_INNER/index.node.gz
+cp release/libcozo_node-$VERSION-x86_64-unknown-linux-gnu.so.gz $NODE_DIR_INNER/cozo_node_prebuilt.gz
 pushd $NODE_DIR
-gunzip 6/index.node.gz
+gunzip 6/cozo_node_prebuilt.gz
 tar cvzf 6-linux-x64.tar.gz 6/
 popd
 
 rm -fr $NODE_DIR_INNER
 mkdir -p $NODE_DIR_INNER
-cp release/libcozo_node-$VERSION-aarch64-unknown-linux-gnu.so.gz $NODE_DIR_INNER/index.node.gz
+cp release/libcozo_node-$VERSION-aarch64-unknown-linux-gnu.so.gz $NODE_DIR_INNER/cozo_node_prebuilt.gz
 pushd $NODE_DIR
-gunzip 6/index.node.gz
+gunzip 6/cozo_node_prebuilt.gz
 tar cvzf 6-linux-arm64.tar.gz 6/
 popd
 
 rm -fr $NODE_DIR_INNER
 mkdir -p $NODE_DIR_INNER
-cp release/libcozo_node-$VERSION-x86_64-pc-windows-msvc.dll.gz $NODE_DIR_INNER/index.node.gz
+cp release/libcozo_node-$VERSION-x86_64-pc-windows-msvc.dll.gz $NODE_DIR_INNER/cozo_node_prebuilt.gz
 pushd $NODE_DIR
-gunzip 6/index.node.gz
+gunzip 6/cozo_node_prebuilt.gz
 tar cvzf 6-win32-x64.tar.gz 6/
 popd
 
